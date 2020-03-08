dfc32b765a23ae8f4c178b1a288cfe0ecf78df59
luchev <luchevz@gmail.com>
2020-03-08 14:46:41 +0000

    Fix user drop down menu autohide
    User drop down from the top navigation now hides when ESC is pressed or when the user clicks outsdie the menu

--- js/nav-user-drop-down.js
+++ js/nav-user-drop-down.js
@@ -1,10 +1,21 @@
-
 let navUser = document.getElementById('nav-user');
 let navUserDropdown = document.getElementById('nav-user-drop-down')
-navUser.addEventListener('click', function () {
-    if (navUserDropdown.style.display == 'none') {
-        navUserDropdown.style.display = 'block';
-    } else {
+
+window.addEventListener('click', function (event) {
+    if (event.target.closest('#nav-user')) {  // If clicked on nav-user
+        if (navUserDropdown.style.display == 'none') {
+            navUserDropdown.style.display = 'block';
+        } else {
+            navUserDropdown.style.display = 'none';
+        }
+    } else {  // If clicked outside of the drop-down -> hide the drop down
+        navUserDropdown.style.display = 'none';
+    }
+});
+
+// Hide dropdown when ESC key is pressed
+window.addEventListener('keydown', function (event) {
+    if (event.which == 27 && navUserDropdown.style.display != 'none') {
         navUserDropdown.style.display = 'none';
     }
 });
