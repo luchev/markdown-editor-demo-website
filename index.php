2ebe977fb45fb946dc65bc214371f35704fcf897
luchev <luchevz@gmail.com>
2020-06-08 19:44:25 +0300

    Add query option which doesn't include any external html files

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php
 
 session_start();
-
 require_once 'php/lib/utils.php';
+
+if (isset($_GET['q'])) {
+    $params = parse_url_params($_GET['q']);
+    $query = $params[0];
+    if (file_exists('php/query/' . $query . '.php')) {
+        include "php/query/$query.php";
+        return;
+    } else {
+        error(404, "Cannot find /q/" . $query . " on this website :(");
+    }
+}
 
 include 'php/lib/header.php';
 include 'php/lib/nav.php';
-
 if (isset($_GET['p'])) {
-    $page = explode("/", $_GET['p'])[0];
+    $params = parse_url_params($_GET['p']);
+    $page = $params[0];
     if (file_exists('php/page/' . $page . '.php')) {
         include "php/page/$page.php";
     } else {
-        error(404, "Cannot find " . $page . " on this website :(");
+        error(404, "Cannot find /p/" . $page . " on this website :(");
     }
 } else {
     include 'php/page/home.php';
 }
-
 include 'php/lib/footer.php';
